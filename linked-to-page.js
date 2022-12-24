a24a455f326e6e78e376e8e5b717951abc8023ef
Emily-Kang77 <109045956+Emily-Kang77@users.noreply.github.com>
2022-12-23 23:45:59 -0800

    Renamed files. Played around w/imports
    #10
    
    I tried: 
    - Importing every... er... method from this module: `import './node_modules/fullcalendar/index.global.js';`
        - Error: `Uncaught ReferenceError: FullCalendar is not defined`
    - Importing just FullCalendar because that's the object whose Calendar method we use to initialize the calendar object: 
        - `import {FullCalendar} from './node_modules/fullcalendar/index.global.js';`
        - `import FullCalendar from './node_modules/fullcalendar/index.global.js';`
        - Error: `Uncaught SyntaxError: ambiguous indirect export: default`
    - Importing Calendar. Same ambiguous indirect export error. 
    - Importing Calendar and changing `new FullCalendar.Calendar` to just `new Calendar`
        - Error: Calendar not defined 
    
    Conclusion: Seriously, seriously read the docs to actually know what object has what method. This is just trying out stuff blindly... I know the calendar could  show up by changing one or two lines, which is exciting, but I should be efficient about this.

---
+++ linked-to-page.js
@@ -0,0 +1,27 @@
+// Import may not work because the module isn't assigned a name?
+// I tried importing everything. But then FullCalendar is not defined.
+// Just importing FullCalendar results in ambiguous export error or something.
+import './node_modules/fullcalendar/index.global.js';
+
+// Event listener that waits until page is loaded to initialize calendar
+document.addEventListener('DOMContentLoaded', function() {
+  // this variable connects to the calendar container in the html
+  var calendarEl = document.getElementById('calendar');
+  // create calendar object. param: <var connected to html, object about options>.
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+
+    // this is an object with objects inside it.
+    headerToolbar: { center: 'dayGridMonth,timeGridWeek' }, // buttons for switching between views
+
+    views: {
+      dayGridMonth: { // name of view
+        titleFormat: { year: 'numeric', month: '2-digit', day: '2-digit' }
+        // other view-specific options here
+      }
+    }
+
+  });
+  // needed to actually show the calendar i guess.
+  calendar.render();
+});
+
